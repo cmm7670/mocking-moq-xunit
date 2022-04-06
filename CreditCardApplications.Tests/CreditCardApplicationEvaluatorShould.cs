a4e2ac11767b0f9ecdf57ae03bc8144edb70d384
cmm7670 <cmm7670@hotmail.com>
2022-04-06 10:55:26 -0500

    completed module 3

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using Moq;
 
 namespace CreditCardApplications.Tests
 {
@@ -8,7 +9,9 @@ namespace CreditCardApplications.Tests
         [Fact]
         public void AcceptHighIncomeApplications()
         {
-            var sut = new CreditCardApplicationEvaluator(null);
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
 
             var application = new CreditCardApplication { GrossAnnualIncome = 100_000 };
 
@@ -20,13 +23,80 @@ namespace CreditCardApplications.Tests
         [Fact]
         public void ReferYoungApplication()
         {
-            var sut = new CreditCardApplicationEvaluator(null);
+            var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+            mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);  // Before adding this the program never evaluated application.Age
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
 
             var application = new CreditCardApplication { Age = 19 };
 
             CreditCardApplicationDecision decision = sut.Evaluate(application);
 
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+
+        [Fact]
+        public void DeclineLowIncomeApplications()
+        {
+            var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+            // mockValidator.Setup(x => x.IsValid("x")).Returns(true);  // Returns true only if the value passed in is "x"
+            // mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);  // Returns true when any string is passed to it
+            // mockValidator.Setup(x => x.IsValid(It.Is<string>(number => number.StartsWith("y")))).Returns(true);  // Returns true when the value passed in starts with "y"
+            // mockValidator.Setup(x => x.IsValid(It.IsInRange("a", "z", Moq.Range.Inclusive))).Returns(true); // Returns true when value passed in is within specified range
+            // mockValidator.Setup(x => x.IsValid(It.IsIn("z", "y", "x"))).Returns(true);  // Returns true when value passed in is in list of acceptable values
+            mockValidator.Setup(x => x.IsValid(It.IsRegex("[a-z]"))).Returns(true);  // Returns true if there is a match on the regular expression
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 19_999,
+                Age = 42,
+                FrequentFlyerNumber = "y"
+            };
+
+            var decision = sut.Evaluate(application);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
+        }
+
+        [Fact]
+        public void ReferInvalidFrequentFlyerApplication()
+        {
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>(MockBehavior.Strict);
+
+            mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(false);
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication();
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+
+        [Fact]
+        public void DeclineLowIncomeApplicationsOutDemo()
+        {
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+            bool isValid = true;    // This is the value that the mocked system will return in the out
+            mockValidator.Setup(x => x.IsValid(It.IsAny<string>(), out isValid));   // The overload version of this method doesn't have a return type so .Returns is not needed
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 19_999,
+                Age = 42
+            };
+
+            CreditCardApplicationDecision decision = sut.EvaluateUsingOut(application);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
         }
     }
 }
